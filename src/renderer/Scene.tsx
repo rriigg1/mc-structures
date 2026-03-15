2513fed8ea2ba927d230407d80de6655118b84c0
rriigg1 <moritzve@gmx.de>
2026-03-15 20:58:52 +0100

    Increase fov of shadow camera to fix shadows for larger builds

--- src/renderer/Scene.tsx
+++ src/renderer/Scene.tsx
@@ -1,26 +1,22 @@
 import { Canvas} from "@react-three/fiber";
 import { Environment, OrbitControls, Stats } from "@react-three/drei";
-import * as THREE from "three";
-import { Camera } from "three";
 
 export function Scene({children}: {children?: React.ReactNode}) {
-    const geometry = new THREE.BoxGeometry(0.25, 4, 0.25)
-
-    geometry.translate(0, 4, 0)
-
-    // test rotations
-    geometry.rotateX(THREE.MathUtils.degToRad(45))
-
-
-    const camera = new Camera()
-    camera.position.set(5, 5, 0)
-    camera.lookAt(0, 0, 0)
-
     return (
         <Canvas shadows camera={{ position: [-10, 10, 3.4]}}>
             <directionalLight
-                position={[15,30,12]}
-                castShadow />
+                position={[45,90,36]}
+                intensity={2}
+                castShadow
+                shadow-mapSize-width={1024}
+                shadow-mapSize-height={1024}
+                shadow-camera-near={0.5}
+                shadow-camera-far={200}
+                shadow-camera-left={-30}
+                shadow-camera-right={30}
+                shadow-camera-top={30}
+                shadow-camera-bottom={-30}>
+            </directionalLight>
             {children}
             <mesh position={[0, -0.5, 0]} rotation={[-Math.PI / 2, 0, 0]} receiveShadow>
                 <planeGeometry args={[100, 100]} />
